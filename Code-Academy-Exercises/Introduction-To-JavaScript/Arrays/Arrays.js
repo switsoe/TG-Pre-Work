67c478c699c58d3c3b96b9d86d8c0c0ad0aba52f
essdubb <switsoe@gmail.com>
2018-10-25 17:31:06 -0600

    Partial JavaScript "Arrays" exercises

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Arrays.js
@@ -0,0 +1,5 @@
+// Discussion of the concept of arrays, identifying the emphasis on lists for data.
+
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
+
+console.log(newYearsResolutions);
