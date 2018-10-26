528760d48030fd41f61bc353ab52cc5492ffd334
essdubb <switsoe@gmail.com>
2018-10-25 22:50:45 -0600

    Completed Arrays Exercises

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/More-Array-Methods.js
@@ -0,0 +1,10 @@
+const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+groceryList.shift();
+
+
+groceryList.unshift("popcorn");
+
+
+const pastaIndex = groceryList.indexOf('pasta');
+console.log(pastaIndex);
